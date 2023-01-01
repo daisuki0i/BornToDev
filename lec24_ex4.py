9291bc8bf318709d13afcab349679061be6d2e99
daisuki0i <daisuki0i@daisuki0is-MacBook-Pro.local>
2023-01-01 23:27:36 +0700

    Change name of the variable

--- lec24_ex4.py
+++ lec24_ex4.py
@@ -1,5 +1,5 @@
 #1
-print("---Your Score : " , 50+78+89+28,"/400 ---")
+print("---Your Score : " , 50+78+89+28," /400 ---")
 print("Foundation English :" ,50)
 print("General Business :" ,78)
 print("Introduction to Computer Systems :" ,89)
@@ -12,7 +12,7 @@ founEng = "Foundation English"
 genBusiness = "General Business"
 introComSys = "Introduction to Computer Systems"
 comPro = "Computer Programming"
-print("---Your Score : " , 50+78+89+28,"/400 ---")
+print("---Your Score : " , 50+78+89+28," /400 ---")
 print(founEng,": " , 50)
 print(genBusiness,": " , 78)
 print(introComSys,": " , 89)
@@ -25,7 +25,7 @@ f = 50
 g = 78
 i = 89
 c = 28
-print("---Your Score : " , f+g+i+c,"/400 ---")
+print("---Your Score : " , f+g+i+c," /400 ---")
 print("Foundation English :" , f)
 print("General Business :" , g)
 print("Introduction to Computer Systems :" , i)
